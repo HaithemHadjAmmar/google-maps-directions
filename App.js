4703eafb6a1b3312d9095dcd2267cfd585d16c7d
Haithem Hadj Ammar <108501803+HaithemHadjAmmar@users.noreply.github.com>
2023-10-05 15:32:17 +0100

    Maps-Directions-react-native-expo

--- App.js
+++ App.js
@@ -1,11 +1,131 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import MapView, { Marker, Polyline } from 'react-native-maps';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [mapLat, setMapLat] = useState(36.8065); // Initial latitude for Tunisia
+  const [mapLong, setMapLong] = useState(10.1815); // Initial longitude for Tunisia
+  const [markers, setMarkers] = useState([]); // State to store markers
+  const [showPolyline, setShowPolyline] = useState(false); // State to show/hide polyline
+  const [distance, setDistance] = useState(null); // State to store distance
+  const [selectedMarkerIndex, setSelectedMarkerIndex] = useState(null); // Index of the selected marker
+
+  useEffect(() => {
+    // Calculate and update the distance whenever markers change
+    if (markers.length === 2) {
+      const lat1 = markers[0].latitude;
+      const lon1 = markers[0].longitude;
+      const lat2 = markers[1].latitude;
+      const lon2 = markers[1].longitude;
+
+      const distanceInKm = calculateDistance(lat1, lon1, lat2, lon2);
+      setDistance(distanceInKm);
+    } else {
+      setDistance(null);
+    }
+  }, [markers]);
+
+  const calculateDistance = (lat1, lon1, lat2, lon2) => {
+    const radius = 6371; // Earth's radius in kilometers
+    const dLat = degToRad(lat2 - lat1);
+    const dLon = degToRad(lon2 - lon1);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(degToRad(lat1)) * Math.cos(degToRad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = radius * c;
+    return distance;
+  };
+
+  const degToRad = (deg) => {
+    return deg * (Math.PI / 180);
+  };
+
+  const handleMapPress = (e) => {
+    const { latitude, longitude } = e.nativeEvent.coordinate;
+    
+    // If a marker is selected, update its position
+    if (selectedMarkerIndex !== null) {
+      const updatedMarkers = [...markers];
+      updatedMarkers[selectedMarkerIndex] = { latitude, longitude };
+      setMarkers(updatedMarkers);
+      setSelectedMarkerIndex(null); // Deselect the marker
+    } else {
+      // Allow adding only two markers
+      if (markers.length < 2) {
+        const newMarker = { latitude, longitude };
+        setMarkers([...markers, newMarker]);
+        if (markers.length === 1) {
+          setShowPolyline(true);
+        }
+      }
+    }
+  };
+
+  const swapMarkers = () => {
+    if (markers.length === 2) {
+      // Swap the positions of the first and second markers
+      const [firstMarker, secondMarker] = markers;
+      setMarkers([secondMarker, firstMarker]);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: mapLat,
+          longitude: mapLong,
+          latitudeDelta: 5, // Adjust the delta values as needed
+          longitudeDelta: 5,
+        }}
+        onPress={handleMapPress} // Handle map clicks
+      >
+        {/* Display markers from state */}
+        {markers.map((marker, index) => (
+          <Marker
+            key={index}
+            coordinate={marker}
+            title={`Marker ${index + 1}`}
+            description={`Latitude: ${marker.latitude}, Longitude: ${marker.longitude}`}
+            onPress={() => setSelectedMarkerIndex(index)} // Select a marker on press
+          />
+        ))}
+
+        {/* Display polyline when two markers are added */}
+        {showPolyline && markers.length === 2 && (
+          <Polyline
+            coordinates={markers}
+            strokeWidth={2}
+            strokeColor="blue"
+          />
+        )}
+      </MapView>
+
+      <View style={styles.infoContainer}>
+        {distance !== null && (
+          <Text style={styles.distanceText}>Distance: {distance.toFixed(2)} km</Text>
+        )}
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.addButton}
+            onPress={() => setMarkers([])} // Clear markers
+          >
+            <Text>Clear Markers</Text>
+          </TouchableOpacity>
+
+          {markers.length === 2 && (
+            <TouchableOpacity
+              style={styles.swapButton}
+              onPress={swapMarkers} // Swap the first and second markers
+            >
+              
+            </TouchableOpacity>
+          )}
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +133,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  map: {
+    flex: 1,
+  },
+  infoContainer: {
+    position: 'absolute',
+    bottom: 16,
+    left: 16,
+    right: 16,
+    backgroundColor: 'white',
+    borderRadius: 8,
+    padding: 8,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  distanceText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+  },
+  addButton: {
+    backgroundColor: 'orange',
+    padding: 8,
+    borderRadius: 4,
+    marginLeft: 8,
   },
 });
